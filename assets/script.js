b51bcd1b87831931e1427dc787ef164fa57bcab8
OmarD <omar.daghestani@colorado.edu>
2022-07-24 12:01:55 -0600

    added first api and adjusted some html + css files

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var getCurrency = function () {
+    // format the github api url
+    var apiUrl = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    });
+  };
+
+  getCurrency();
+
+// var getCurrency = function (Currency1, Currency2) {
+//   // format the github api url
+//   var apiUrl = `"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${Currency1}/${Currency2}.json"`;
+
+//   // make a request to the url
+//   fetch(apiUrl).then(function (response) {
+//     response.json().then(function (data) {
+//       console.log(data, Currency1, Currency2);
+//     });
+//   });
+// };
+
+// getCurrency();
